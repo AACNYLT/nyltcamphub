c31b31ae6613252d286b2f7577ef6795a8242e03
Aroon Narayanan <aroon@gatech.edu>
2020-12-12 19:06:14 -0500

    Removed old files and added react app

--- server.ts
+++ server.ts
@@ -14,6 +14,8 @@ mongoose.connect(DB_URL, {useNewUrlParser: true})
 
 app.use('/api', Routes);
 
+app.use('/', express.static('./static/build'));
+
 const server = app.listen(process.env.PORT || 80, () => {
     // @ts-ignore
     console.log(`Server available on port ${server.address().port}`);
